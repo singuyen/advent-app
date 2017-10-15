39451e8ea2f042f1a2b9de36f85b203061d83ba8
Si Nguyen <nguyensi.art@gmail.com>
2017-10-15 23:20:47 +1100

    Fixed typo

--- src/container/Intro.js
+++ src/container/Intro.js
@@ -15,14 +15,14 @@ class Intro extends Component {
             <li>[YELLOW] block is Easter Bunny HQ (Puzzle 1)</li>
             <li>----</li>
             <li>Puzzle 1: Easter Bunny HQ is <strong>{awayFromDestination} blocks</strong> away (Yellow)</li>
-            <li>Puzzle 2: First location I visit twice <strong>{awayFromHQ} blocks</strong> away (red)</li>
+            <li>Puzzle 2: First location I visit twice is <strong>{awayFromHQ} blocks</strong> away (red)</li>
           </ul>
           <h3>Solution</h3>
           <ul className="result-container">
             <li>
               <h5>Create mapData</h5>
-              <small>It's important that I need to turn the inputString into the data that I can use to calculate.</small>
-              <p>"It's a lot easier if I can just move UP, DOWN, LEFT, RIGHT instead of just L/R".</p> 
+              <small>It's important to turn inputString into useful data.</small>
+              <p>"It's a lot easier with UP, DOWN, LEFT, RIGHT instead of just L/R".</p> 
               <p>"I need to capture my every single move in <strong>path</strong>"</p>
               <p>"I know how big my Map is after I captured <strong>minX, minY, maxY, maxY</strong>"</p>
             </li>
